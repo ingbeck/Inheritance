7064b01d27c85b617e27651448a8cba103737587
ingobecker <mail@ingobecker.de>
2024-01-31 10:58:21 +0100

    create class Vehicle.java

---
+++ src/main/java/com/github/ingbeck/Vehicle.java
@@ -0,0 +1,16 @@
+package com.github.ingbeck;
+
+public class Vehicle {
+
+    private String manufacturer;
+    private String model;
+    private int yearOfManufacture;
+
+
+    public Vehicle(String manufacturer, String model, int yearOfManufacture) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+}
